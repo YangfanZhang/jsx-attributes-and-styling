59da0a53a2e3f0dbd0bb62a0a8451f9d0d27a68f
Yangfan Zhang <42492049+YangfanZhang@users.noreply.github.com>
2020-07-06 15:26:01 +1000

    Added food imgs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading">My Favourite Foods</h1>
+    <div>
+      <img
+        alt="food-images"
+        className="food-img"
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg"
+      />
+      <img
+        alt="food-images"
+        className="food-img"
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg"
+      />
+      <img
+        alt="food-images"
+        className="food-img"
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
